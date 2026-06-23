540a8760203c5bc0a99b624de2a8f8727e28d568
mariomonir <mario3monir@gmail.com>
2026-06-23 11:16:34 +0200

    chore: disable admin route until GitHub token is configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Project from "@/pages/Project";
-import Admin from "@/pages/Admin";
+// import Admin from "@/pages/Admin";
 
 export default function App() {
   return (
@@ -9,7 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:dir" element={<Project />} />
-        <Route path="/admin" element={<Admin />} />
+        {/* <Route path="/admin" element={<Admin />} /> */}
       </Routes>
     </HashRouter>
   );
